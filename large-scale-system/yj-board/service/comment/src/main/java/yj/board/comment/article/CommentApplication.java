341f4b2b1ccb8b0a01d7b702f44d61704bdc87eb
songu1 <syj0002012@naver.com>
2025-02-26 03:16:51 +0900

    Spring Boot 프로젝트 세팅

---
+++ large-scale-system/yj-board/service/comment/src/main/java/yj/board/comment/article/CommentApplication.java
@@ -0,0 +1,11 @@
+package yj.board.comment.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CommentApplication.class, args);
+    }
+}
